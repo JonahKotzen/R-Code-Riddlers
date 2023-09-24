99a0bc82d2fdf7f91f2d18a20084e4f1232029d1
Ben Riccio <benriccio@bens-air-3.bc.edu>
2023-09-24 12:17:10 -0400

    question 4 done

--- R-Code-Riddlers_module3.R
+++ R-Code-Riddlers_module3.R
@@ -4,13 +4,17 @@ library(dplyr)
 library(ape)
 library(phytools)
 library(caper)
+library(nlme)
+library(MuMIn)
+library(viridis)
+library(geiger)
 
 # Question 1: Load Files as Tibbles
 anole.dat <- read.csv("anole.dat.csv")
 anole.eco <- read.csv("anole.eco.csv")
 anole.tre <- read.tree("anole.tre")
 
-#Merge Data
+#Merge Data and Log Transform
 anole.log <- anole.dat %>%
   left_join(anole.eco) %>% 
   filter(!Ecomorph %in%c("U","CH")) %>%
@@ -43,14 +47,13 @@ plot2 <- ggplot(anole.log, aes(x = ArbPD, y = residuals_PD)) +
 plot2
 
 #Question 4: Phylogenetics
-pgls_PH <- pgls(log(HTotal) ~ log(PH), phy = anole.tre)
-pgls_PD <- pgls(log(HTotal) ~ log(ArbPD), phy = anole.tre)
-pgls_PH_PD <- pgls(log(HTotal) ~ log(PH) + log(ArbPD), phy = anole.tre)
+pgls_PH <- gls(HTotal~SVL + PH, correlation = corBrownian(1,phy = anole.tre,form=~Species),data = anole.log, method = "ML")
+pgls_PD <- gls(HTotal~SVL + ArbPD, correlation = corBrownian(1,phy = anole.tre,form=~Species),data = anole.log, method = "ML")
+pgls_PH_PD <- gls(HTotal~SVL + PH+ArbPD, correlation = corBrownian(1,phy = anole.tre,form=~Species),data = anole.log, method = "ML")
 
 #Question 5: Model Assessment (AICc & AICw)
-aic_PH <- AICc(pgls_PH)
-aic_PD <- AICc(pgls_PD)
-aic_PH_PD <- AICc(pgls_PH_PD)
+anole.phylo.aic <- AICc(pgls_PH,pgls_PD,pgls_PH_PD)
+aicw(anole.phylo.aic$AICc)
 
 #Question 6: Plot & PGLS 
 
